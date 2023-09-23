040fde3363708644a639f1e9dd139096bc2452dd
Daniel Levy <daniellofficial@gmail.com>
2023-09-23 17:12:22 +0300

    fix for animation

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,26 +1,19 @@
-import { ToyPreview } from './ToyPreview.jsx'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { ToyPreview } from "./ToyPreview.jsx"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 
 export function ToyList({ toys, onRemoveToy }) {
-
-    return (
-      
-<TransitionGroup component="div" className="toy-list">
-
-{toys.map(toy => (
-
-  <CSSTransition 
-    key={toy._id} 
-    timeout={150}
-    classNames="toy"
-    unmountOnExit
-    appear
-  >
-    <ToyPreview toy={toy} onRemoveToy={onRemoveToy} />
-  </CSSTransition>
-
-))}
-
-</TransitionGroup>
-    )
+  return (
+    <TransitionGroup component={null}>
+    <section className="toy-list">
+      {toys.map((toy) => (
+        <CSSTransition
+          key={toy._id}
+          timeout={500}
+        >
+          <ToyPreview toy={toy} onRemoveToy={onRemoveToy} />
+        </CSSTransition>
+      ))}
+    </section>
+    </TransitionGroup>
+  )
 }
