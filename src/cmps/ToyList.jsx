f1427f2e2d571c64dc538986c246214c4a0b77ca
Daniel Levy <daniellofficial@gmail.com>
2023-09-23 17:25:46 +0300

    F/ potintel fix for preview css

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,19 +1,12 @@
 import { ToyPreview } from "./ToyPreview.jsx"
-import { CSSTransition, TransitionGroup } from "react-transition-group"
 
 export function ToyList({ toys, onRemoveToy }) {
   return (
-    <TransitionGroup component={null}>
     <section className="toy-list">
       {toys.map((toy) => (
-        <CSSTransition
-          key={toy._id}
-          timeout={500}
-        >
-          <ToyPreview toy={toy} onRemoveToy={onRemoveToy} />
-        </CSSTransition>
+        <ToyPreview key={toy._id} toy={toy} onRemoveToy={onRemoveToy} />
       ))}
     </section>
-    </TransitionGroup>
+
   )
 }
